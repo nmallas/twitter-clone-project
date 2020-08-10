6b0df0a0225e8645405b615cc34d953fb1af4bac
Nick Mallas <nmallas23@gmail.com>
2020-08-10 00:07:52 -0500

    added user sign up and authentication with jwt

--- express-apis-frontend/index.js
+++ express-apis-frontend/index.js
@@ -8,9 +8,13 @@ const app = express();
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
-// Define a route.
+
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/sign-up", (req, res) => {
+  res.render("sign-up");
 });
 
 // Define a port and start listening for connections.
